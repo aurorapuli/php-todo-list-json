27139e4a44ed7d2ec9db21ed8f0981936fc22d3b
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2024-01-25 19:16:01 +0100

    add: todo list

---
+++ Back/List.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$toDo = [
+    [
+        'task' => 'Fare la spesa',
+        'completed' => true
+    ],
+    [
+        'task' => 'Studiare PHP',
+        'completed' => true
+    ],
+    [
+        'task' => 'Bere il Thè',
+        'completed' => true
+    ],
+    [
+        'task' => 'Guardare Netflix',
+        'completed' => true
+    ],
+];
